da10a1d87870c9f7bf467d6b5d75e776137f04ab
lucasferreiralimax <lucasferreiralimax@gmail.com>
2022-02-01 04:00:56 -0800

    remove backets in class name

--- app/controllers/slide-content.js
+++ app/controllers/slide-content.js
@@ -1,5 +1,3 @@
 import Controller from '@ember/controller';
 
-export default class SlideContentController extends Controller {
-
-}
+export default class SlideContentController extends Controller {}
