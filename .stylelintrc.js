5fa2aa6f80fb54f24109e625b4994ccca22453b3
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-06-14 13:23:28 -0300

    adjustment lint js in config stylelintrc.js

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -3,14 +3,14 @@
 module.exports = {
   extends: 'stylelint-config-standard-scss',
   rules: {
-    "no-descending-specificity": null,
-    "no-invalid-position-at-import-rule": null,
-    "scss/load-no-partial-leading-underscore": null,
-    "media-feature-name-no-unknown": null,
-    "declaration-block-no-shorthand-property-overrides": null,
-    "font-family-no-missing-generic-family-keyword": null,
-    "scss/no-duplicate-mixins": null,
-    "scss/at-mixin-pattern": null,
-    "keyframes-name-pattern": null
-  }
+    'no-descending-specificity': null,
+    'no-invalid-position-at-import-rule': null,
+    'scss/load-no-partial-leading-underscore': null,
+    'media-feature-name-no-unknown': null,
+    'declaration-block-no-shorthand-property-overrides': null,
+    'font-family-no-missing-generic-family-keyword': null,
+    'scss/no-duplicate-mixins': null,
+    'scss/at-mixin-pattern': null,
+    'keyframes-name-pattern': null,
+  },
 };
