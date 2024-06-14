2a6e6b018febc79440e1f28a376d7a87fe1e251a
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-06-14 13:20:27 -0300

    fix: adjustment stylelint-config-standard-scss with scss

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,5 +1,16 @@
 'use strict';
 
 module.exports = {
-  extends: ['stylelint-config-standard', 'stylelint-prettier/recommended'],
+  extends: 'stylelint-config-standard-scss',
+  rules: {
+    "no-descending-specificity": null,
+    "no-invalid-position-at-import-rule": null,
+    "scss/load-no-partial-leading-underscore": null,
+    "media-feature-name-no-unknown": null,
+    "declaration-block-no-shorthand-property-overrides": null,
+    "font-family-no-missing-generic-family-keyword": null,
+    "scss/no-duplicate-mixins": null,
+    "scss/at-mixin-pattern": null,
+    "keyframes-name-pattern": null
+  }
 };
