68ba4771718607c43f0f030c3b242facec0b1c0d
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-06-14 12:20:58 -0300

    sass in project

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,9 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
-    // Add options here
+    sassOptions: {
+      extension: 'scss'
+    }
   });
 
   return app.toTree();
